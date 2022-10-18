50b4f5291e22bafe2adcc9365c6d3c2186e06661
nicholasz-sun <nicholas.sun@sjsu.edu>
2022-10-18 16:48:36 -0700

    added button

--- sketch.js
+++ sketch.js
@@ -28,16 +28,21 @@ let randomIndex;
 let counter = 0;
 let changeBackground;
 let animating = false;
+let button;
+
 function setup() {
-  createCanvas(1080, 1080);
+  createCanvas(512, 512);
   background(230);
-  fill(0,102,153);
-  textSize (32);
-
-
+  fill(0, 102, 153);
+  textSize(32);
   text("Click to Randomize", 150, y);
 
   setTimeout(changeBackground, 1000);
+
+  //Button For randomizer
+  button = createButton("Click to Randomize");
+  button.mousePressed(buttonPressed);
+  button.class("randomizerButton");
 
 }
 
@@ -49,8 +54,8 @@ function display() {
   //if animating is true then ellipses will be drawn on the screen
   // two equal signs are used to check if the function "animating" is true or not
   // using one equal signs means you are assigning the funtion "animating" to either true or false
-  if (animating == true){
-    fill(random(255), random(100,200), random(100))
+  if (animating == true) {
+    fill(random(255), random(100, 200), random(100))
     ellipse(random(width), random(height), random(50, 200))
   }
 }
@@ -70,7 +75,7 @@ function randomizer() {
   }
 }
 
-function mousePressed() {
+function buttonPressed() {
   animating = true;
   //Once user clicks 2 seconds will pass before text changes
   setTimeout(randomizer, 2000);
